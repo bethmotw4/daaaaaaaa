a79988f2444179c18219c7182237bea480aadbab
森本 義基 <yoshiki.morimoto@rakus.internal>
2020-07-09 12:13:38 +0900

    first commit

---
+++ src/main/java/com/example/demo/DaaaaaaaaaaaaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaaaaaaaaaaaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaaaaaaaaaaaaApplication.class, args);
+	}
+
+}
