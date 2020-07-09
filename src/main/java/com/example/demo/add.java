f21e1d24e9b2d61f4c9fb975f4d4a3b21e9adaa1
cat-is-so-cute <ryosuke.nakanishi@rakus.co.jp>
2020-07-09 12:21:02 +0900

    くそコード追加

--- src/main/java/com/example/demo/add.java
+++ src/main/java/com/example/demo/add.java
@@ -1,5 +1,13 @@
 package com.example.demo;
 
 public class add {
-
+	public static void main(String[] args) {
+		int speed = 40;
+		
+		for (int i = 0;i < 100;i++) {
+			System.out.println("くそコード");
+		}
+		
+		System.out.println("現在の速度: " + speed);
+	}
 }
